dfad384f54e8e7bdacacaecb55ac315a1e13c814
Mateo Gianolio <gianoliomateo@gmail.com>
2016-09-05 02:15:33 +0200

    tabs to spaces for js

--- test.js
+++ test.js
@@ -1,22 +1,22 @@
 (function () {
-	'use strict';
+  'use strict';
 
-	var plt = require('./build/Release/matplotlib');
+  var plt = require('./build/Release/matplotlib');
 
-	var x = new Array(100)
-		.fill(0)
-		.map((x, i) => i / Math.PI);
+  var x = new Array(100)
+    .fill(0)
+    .map((x, i) => i / Math.PI);
 
-	plt.subplot("211");
-	plt.plot(x, x.map(Math.sin), 'color=r', 'label=sin(x)');
-	plt.plot(x, x.map(Math.cos), 'color=g', 'label=cos(x)');
-	plt.legend();
+  plt.subplot("211");
+  plt.plot(x, x.map(Math.sin), 'color=r', 'label=sin(x)');
+  plt.plot(x, x.map(Math.cos), 'color=g', 'label=cos(x)');
+  plt.legend();
 
-	plt.subplot("212");
-	plt.plot(x, x.map(Math.tan), 'color=b', 'label=tan(x)', 'marker=o', 'linestyle=None');
-	plt.legend();
-	plt.grid(true);
-	plt.ylim(-5, 5);
+  plt.subplot("212");
+  plt.plot(x, x.map(Math.tan), 'color=b', 'label=tan(x)', 'marker=o', 'linestyle=None');
+  plt.legend();
+  plt.grid(true);
+  plt.ylim(-5, 5);
 
   plt.save("./examples/subplot.png");
 }());
